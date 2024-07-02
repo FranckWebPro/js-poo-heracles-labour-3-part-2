81d935259fdf70149b15c94e81b2241856e2d117
Franck <contact@franckwebpro.com>
2024-07-02 20:28:09 +0200

    get distance method

--- src/Arena.js
+++ src/Arena.js
@@ -4,4 +4,10 @@ class Arena {
     this.hero = hero;
     this.size = size;
   }
+
+  getDistance(fighter1, fighter2) {
+    const distanceBtwnFigthers = Math.sqrt(Math.pow((fighter1.x - fighter2.x), 2) + Math.pow((fighter1.y - fighter2.y), 2))
+
+    return distanceBtwnFigthers.toFixed(2);
+  }
 }
